52632a9cb0cb7697bd3c521b0d2051bc2417bbb2
Vicent Bellés <vicentbelles@gmail.com>
2022-12-03 11:14:38 +0100

    Set defaultScale to 3 Rendering 3D to texture Added tower obj sample

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -14,8 +14,8 @@
 #include "raylib.h"
 
 #if defined(PLATFORM_WEB)
-    #define CUSTOM_MODAL_DIALOGS            // Force custom modal dialogs usage
-    #include <emscripten/emscripten.h>      // Emscripten library - LLVM to JavaScript compiler
+#define CUSTOM_MODAL_DIALOGS            // Force custom modal dialogs usage
+#include <emscripten/emscripten.h>      // Emscripten library - LLVM to JavaScript compiler
 #endif
 
 #include <stdio.h>                          // Required for: printf()
@@ -29,19 +29,19 @@
 // NOTE: Avoiding those calls, also avoids const strings memory usage
 #define SUPPORT_LOG_INFO
 #if defined(SUPPORT_LOG_INFO)
-    #define LOG(...) printf(__VA_ARGS__)
+#define LOG(...) printf(__VA_ARGS__)
 #else
-    #define LOG(...)
+#define LOG(...)
 #endif
 
 //----------------------------------------------------------------------------------
 // Types and Structures Definition
 //----------------------------------------------------------------------------------
-typedef enum { 
-    SCREEN_LOGO = 0, 
-    SCREEN_TITLE, 
-    SCREEN_GAMEPLAY, 
-    SCREEN_ENDING
+typedef enum {
+	SCREEN_LOGO = 0,
+	SCREEN_TITLE,
+	SCREEN_GAMEPLAY,
+	SCREEN_ENDING
 } GameScreen;
 
 // TODO: Define your custom data types here
@@ -52,7 +52,7 @@ typedef enum {
 static const int screenWidth = 256;
 static const int screenHeight = 256;
 
-static unsigned int screenScale = 1; 
+static unsigned int screenScale = 3;
 static unsigned int prevScreenScale = 1;
 
 static RenderTexture2D target = { 0 };  // Initialized at init
@@ -70,43 +70,43 @@ static void UpdateDrawFrame(void);      // Update and Draw one frame
 int main(void)
 {
 #if !defined(_DEBUG)
-    SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messsages
+	SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messsages
 #endif
 
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "raylib 9yr gamejam");
-    
-    // TODO: Load resources / Initialize variables at this point
-    
-    // Render texture to draw full screen, enables screen scaling
-    // NOTE: If screen is scaled, mouse input should be scaled proportionally
-    target = LoadRenderTexture(screenWidth, screenHeight);
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	InitWindow(screenWidth, screenHeight, "raylib 9yr gamejam");
+
+	// TODO: Load resources / Initialize variables at this point
+
+	// Render texture to draw full screen, enables screen scaling
+	// NOTE: If screen is scaled, mouse input should be scaled proportionally
+	target = LoadRenderTexture(screenWidth, screenHeight);
+	SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+	emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);     // Set our game frames-per-second
-    //--------------------------------------------------------------------------------------
+	SetTargetFPS(60);     // Set our game frames-per-second
+	//--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button
-    {
-        UpdateDrawFrame();
-    }
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button
+	{
+		UpdateDrawFrame();
+	}
 #endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadRenderTexture(target);
-    
-    // TODO: Unload all loaded resources at this point
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	UnloadRenderTexture(target);
 
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+	// TODO: Unload all loaded resources at this point
 
-    return 0;
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
 }
 
 //--------------------------------------------------------------------------------------------
@@ -115,49 +115,82 @@ int main(void)
 // Update and draw frame
 void UpdateDrawFrame(void)
 {
-    // Update
-    //----------------------------------------------------------------------------------
-    // Screen scale logic (x2)
-    if (IsKeyPressed(KEY_ONE)) screenScale = 1;
-    else if (IsKeyPressed(KEY_TWO)) screenScale = 2;
-    else if (IsKeyPressed(KEY_THREE)) screenScale = 3;
-    
-    if (screenScale != prevScreenScale)
-    {
-        // Scale window to fit the scaled render texture
-        SetWindowSize(screenWidth*screenScale, screenHeight*screenScale);
-        
-        // Scale mouse proportionally to keep input logic inside the 256x256 screen limits
-        SetMouseScale(1.0f/(float)screenScale, 1.0f/(float)screenScale);
-        
-        prevScreenScale = screenScale;
-    }
 
-    // TODO: Update variables / Implement example logic at this point
-    //----------------------------------------------------------------------------------
+	static init = false;
+	static Camera camera = { 0 };
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    // Render all screen to texture (for scaling)
-    BeginTextureMode(target);
-        ClearBackground(RAYWHITE);
-        
-        // TODO: Draw screen at 256x256
-        DrawRectangle(10, 10, screenWidth - 20, screenHeight - 20, SKYBLUE);
-        
-    EndTextureMode();
-    
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        
-        // Draw render texture to screen scaled as required
-        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, (Rectangle){ 0, 0, (float)target.texture.width*screenScale, (float)target.texture.height*screenScale }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+	static Model tower;
+	static Texture2D texture;
+	static Vector3 towerPos = { 0.0f, 0.0f, 0.0f };                        // Set model position
 
-        // Draw equivalent mouse position on the target render-texture
-        DrawCircleLines(GetMouseX(), GetMouseY(), 10, MAROON);
+	if (!init) {
+		init = true;
+		camera.position = (Vector3){ 200.0f, 200.0f, 200.0f }; // Camera position
+		camera.target = (Vector3){ 0.0f, 8.0f, 0.0f };      // Camera looking at point
+		camera.up = (Vector3){ 0.0f, 1.6f, 0.0f };          // Camera up vector (rotation towards target)
+		camera.fovy = 45.0f;                                // Camera field-of-view Y
+		camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-        // TODO: Draw everything that requires to be drawn at this point:
+		tower = LoadModel("resources/turret.obj");
+		texture = LoadTexture("resources/turret_diffuse.png"); // Load model texture
+		tower.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;            // Set model diffuse texture
 
-    EndDrawing();
-    //----------------------------------------------------------------------------------  
+		BoundingBox towerBBox = GetMeshBoundingBox(tower.meshes[0]);    // Get m
+		SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
+	}
+
+
+	// Update
+	//----------------------------------------------------------------------------------
+	// Screen scale logic (x2)
+	if (IsKeyPressed(KEY_ONE)) screenScale = 1;
+	else if (IsKeyPressed(KEY_TWO)) screenScale = 2;
+	else if (IsKeyPressed(KEY_THREE)) screenScale = 3;
+
+	if (screenScale != prevScreenScale)
+	{
+		// Scale window to fit the scaled render texture
+		SetWindowSize(screenWidth * screenScale, screenHeight * screenScale);
+
+		// Scale mouse proportionally to keep input logic inside the 256x256 screen limits
+		SetMouseScale(1.0f / (float)screenScale, 1.0f / (float)screenScale);
+
+		prevScreenScale = screenScale;
+	}
+
+	UpdateCamera(&camera);
+
+	// TODO: Update variables / Implement example logic at this point
+	//----------------------------------------------------------------------------------
+
+	// Draw
+	//----------------------------------------------------------------------------------
+	// Render all screen to texture (for scaling)
+	BeginTextureMode(target);
+	{
+
+		BeginMode3D(camera);
+		ClearBackground(RAYWHITE);
+
+
+		DrawModel(tower, towerPos, 10.0f, WHITE);
+		DrawGrid(10, 10.0f);
+
+		EndMode3D();
+
+		DrawCircleLines(GetMouseX(), GetMouseY(), 10, MAROON);
+
+	}
+	EndTextureMode();
+
+	BeginDrawing();
+	{
+		ClearBackground(RAYWHITE);
+
+		// Draw render texture to screen scaled as required
+		DrawTexturePro(target.texture, (Rectangle) { 0, 0, (float)target.texture.width, -(float)target.texture.height }, (Rectangle) { 0, 0, (float)target.texture.width* screenScale, (float)target.texture.height* screenScale }, (Vector2) { 0, 0 }, 0.0f, WHITE);
+
+	}
+	EndDrawing();
+	//----------------------------------------------------------------------------------  
 }
