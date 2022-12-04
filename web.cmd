547b58a9ab731e5ab319f2b430002647598ac048
Vicent Bellés <vicentbelles@gmail.com>
2022-12-04 18:53:34 +0100

    Update web.cmd

--- web.cmd
+++ web.cmd
@@ -3,5 +3,5 @@ mkdir build-web
 cd build-web
 emcmake cmake -G "MinGW Makefiles" .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"  && ^
 emmake make && ^
-echo Starting server http://localhost:8000/256_unexpected.html && ^
+echo Starting server http://localhost:8000/raylib_jam_256_unexpected.html && ^
 python -m http.server 8000
