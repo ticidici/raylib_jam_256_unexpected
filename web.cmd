bb6453a3205327fb760859d3fc02164b7700c4a6
Vicent Bellés <vicentbelles@gmail.com>
2022-12-04 18:49:27 +0100

    Moved from Visual Studio randomness to good reliable CMake

---
+++ web.cmd
@@ -0,0 +1,7 @@
+@echo on
+mkdir build-web
+cd build-web
+emcmake cmake -G "MinGW Makefiles" .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"  && ^
+emmake make && ^
+echo Starting server http://localhost:8000/256_unexpected.html && ^
+python -m http.server 8000
