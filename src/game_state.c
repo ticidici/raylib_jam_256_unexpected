48ab82f447c627dbc24b93b75a346a32e55cc553
Vicent Bellés <vicentbelles@gmail.com>
2022-12-09 22:41:31 +0100

    Basic wave spawn logic

--- src/game_state.c
+++ src/game_state.c
@@ -1,4 +1,6 @@
 #include "game_state.h"
+#include "enemy.h"
+#include "terrain.h"
 
 static bool isPaused = false;
 
@@ -27,6 +29,7 @@ static float rangedEnemyUnlockTime = 400;
 static bool bossSpawned = false;
 static float bossSpawnTime = 900;
 
+static EnemyWave wave;
 
 void GameStateInit()
 {
@@ -41,6 +44,10 @@ void GameStateInit()
 	thunderboltNextTime = 0;
 
 	bossSpawned = false;
+
+	wave.startTime = 0;
+	wave.duration = 3;
+	wave.enemiesCount = 0;
 }
 
 void GameStateUpdate()
@@ -50,6 +57,41 @@ void GameStateUpdate()
 	if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_P))
 	{
 		isPaused = !isPaused;
+	}
+
+	if (runTimePassed - wave.startTime >= wave.duration)
+	{
+		// Next wave
+		wave.duration = 3;
+		wave.enemiesCount += 1;
+		wave.startTime = runTimePassed;
+
+		for (int i = 0; i < wave.enemiesCount; i++)
+		{
+			Tile* freeSpawnTiles[BATTLEFIELD_SIZE * BATTLEFIELD_SIZE] = { 0 };
+			int freeSpawnTilesCount = 0;
+			for (int x = 0; x < BATTLEFIELD_SIZE; x++)
+			{
+				for (int y = 0; y < BATTLEFIELD_SIZE; y++)
+				{
+					if (x == 0 || x == BATTLEFIELD_SIZE - 1 || y == 0 || y == BATTLEFIELD_SIZE - 1)
+					{
+						Tile* tile = TerrainGetTile(x, y);
+						if (!tile->enemy)
+						{
+							freeSpawnTiles[freeSpawnTilesCount] = TerrainGetTile(x, y);
+							freeSpawnTilesCount++;
+						}
+					}
+				}
+			}
+			if (freeSpawnTilesCount > 0)
+			{
+				int tileIndex = GetRandomValue(0, freeSpawnTilesCount - 1);
+				Tile* spawnTile = freeSpawnTiles[tileIndex];
+				EnemySpawn(spawnTile->coordX, spawnTile->coordY);
+			}
+		}
 	}
 }
 
@@ -65,10 +107,14 @@ int GetMoney()
 
 int GetResource(TileType tileType)
 {
-	if (tileType == WheatType) return wheat;
-	else if (tileType == WoodType) return wood;
-	else if (tileType == ClayType) return clay;
-	else if (tileType == LavaType) return iron;
+	if (tileType == WheatType)
+		return wheat;
+	else if (tileType == WoodType)
+		return wood;
+	else if (tileType == ClayType)
+		return clay;
+	else if (tileType == LavaType)
+		return iron;
 	return 0;
 }
 
@@ -80,8 +126,10 @@ float GetRunTime()
 void ModifyMoney(int value)
 {
 	money += value;
-	if (money > 999) money = 999;
-	else if (money < 0) money = 0;
+	if (money > 999)
+		money = 999;
+	else if (money < 0)
+		money = 0;
 }
 
 void ModifyResource(TileType tileType, int value)
@@ -89,26 +137,34 @@ void ModifyResource(TileType tileType, int value)
 	if (tileType == WheatType)
 	{
 		wheat += value;
-		if (wheat > 999) wheat = 999;
-		else if (wheat < 0) wheat = 0;
+		if (wheat > 999)
+			wheat = 999;
+		else if (wheat < 0)
+			wheat = 0;
 	}
 	else if (tileType == WoodType)
 	{
 		wood += value;
-		if (wood > 999) wood = 999;
-		else if (wood < 0) wood = 0;
+		if (wood > 999)
+			wood = 999;
+		else if (wood < 0)
+			wood = 0;
 	}
 	else if (tileType == ClayType)
 	{
 		clay += value;
-		if (clay > 999) clay = 999;
-		else if (clay < 0) clay = 0;
+		if (clay > 999)
+			clay = 999;
+		else if (clay < 0)
+			clay = 0;
 	}
 	else if (tileType == LavaType)
 	{
 		iron += value;
-		if (iron > 999) iron = 999;
-		else if (iron < 0) iron = 0;
+		if (iron > 999)
+			iron = 999;
+		else if (iron < 0)
+			iron = 0;
 	}
 }
 
@@ -124,35 +180,41 @@ Vector2 GetThunderboltDamage()
 
 bool IsThunderboltUnlocked()
 {
-	if (runsFinished > 0 || runTimePassed >= thunderboltUnlockTime) return true;
+	if (runsFinished > 0 || runTimePassed >= thunderboltUnlockTime)
+		return true;
 	return false;
 }
 
 bool IsLavaUnlocked()
 {
-	if (runsFinished > 0 || runTimePassed >= lavaUnlockTime) return true;
+	if (runsFinished > 0 || runTimePassed >= lavaUnlockTime)
+		return true;
 	return false;
 }
 
 bool IsFastEnemyUnlocked()
 {
-	if (runTimePassed >= fastEnemyUnlockTime) return true;
+	if (runTimePassed >= fastEnemyUnlockTime)
+		return true;
 	return false;
 }
 
 bool IsRangedEnemyUnlocked()
 {
-	if (runTimePassed >= rangedEnemyUnlockTime) return true;
+	if (runTimePassed >= rangedEnemyUnlockTime)
+		return true;
 	return false;
 }
 
 bool ShouldSpawnBoss()
 {
-	if (bossSpawned) return false;
-	
-	if (runTimePassed >= bossSpawnTime) return true;
+	if (bossSpawned)
+		return false;
 
-	return false; 
+	if (runTimePassed >= bossSpawnTime)
+		return true;
+
+	return false;
 }
 
 void SetBossSpawned()
